5cf4f370ba3f2c0ea5a551d1f7028cab0269f852
Ashley Arthur <ashleyarthur98@gmail.com>
2024-09-30 09:29:52 +0100

    testing

--- default.nix
+++ default.nix
@@ -10,35 +10,48 @@ let
   pkgs_old = (import (builtins.fetchGit {
       name = "glibc_2_18";
       url = "https://github.com/NixOS/nixpkgs/";
-      ref = "refs/heads/nixos-19.09";
-      rev = "b79f64b5eb5fa8ca2f844ddb4d7c186b6c69a293";
+      ref = "refs/heads/nixos-18.09";
+      rev = "6a3f5bcb061e1822f50e299f5616a0731636e4e7";
   }) { });
+
+    libc_old = pkgs_old.glibc.overrideAttrs (p:{
+       pname = "glibc";
+    });
 
     stdenv_old = (pkgs.overrideCC pkgs.stdenv (pkgs.wrapCCWith {
 	  cc = pkgs_old.gcc-unwrapped;
 	  bintools = pkgs.wrapBintoolsWith {
 	    bintools = pkgs.binutils-unwrapped;
-	    libc = pkgs_old.glibc;
+	    libc = libc_old;
 	    };
     }));
 
+  pkgs2 = import <nixpkgs> { 
+    overlays = [ (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/2e7ccf572ce0f0547d4cf4426de4482936882d0e.tar.gz")) ];
+    config.replaceStdenv = { pkgs, ... }: stdenv_old;
+  };
+
+  rustbin = pkgs2.rust-bin.stable.latest.default.override {
+	targets = [ "x86_64-unknown-linux-gnu" ];
+  };
+
   rustPlatform = pkgs.makeRustPlatform {
-    rustc = pkgs.rust-bin.stable.latest.default;
-    cargo = pkgs.rust-bin.stable.latest.default;
+    rustc = rustbin;
+    cargo = rustbin;
     stdenv = stdenv_old;
   };
-  fs =  (pkgs.buildPackages.callPackage ./freeswitch { });
+  #fs =  (pkgs.buildPackages.callPackage ./freeswitch { });
 in
 rustPlatform.buildRustPackage rec {  
   pname = "freeswitch_rs";
   version = "0.1";
   nativeBuildInputs = with pkgs; [ 
-    fs
+    #fs
     rustPlatform.bindgenHook
   ] ++ lib.optionals stdenv.isDarwin [
   ];
 
-  NIX_CFLAGS_COMPILE="-isystem ${fs.out}/include/freeswitch";
+  #NIX_CFLAGS_COMPILE="-isystem ${fs.out}/include/freeswitch";
 
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
